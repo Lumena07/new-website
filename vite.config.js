13f9025d8eec835c983e1f9ae3db31efe74ec559
Lumena07 <alycelumena@gmail.com>
2024-11-07 16:02:33 +0300

    changed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: './',
+  assetsInclude: ['**/*.jpg', '**/*.png', '**/*.jpeg', '**/*.svg'], // if needed
 })
